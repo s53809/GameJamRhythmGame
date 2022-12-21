b13eee7c98fa55da439b9e98783f9c891bb9d3e3
Ozzangeo <Ozzangeo0523@gmail.com>
2022-12-22 01:10:52 +0900

    Rhythm Main 40% Complete

--- RhythmGame/Assets/Scripts/Ozi's Scene/Note/NoteSpawner.cs
+++ RhythmGame/Assets/Scripts/Ozi's Scene/Note/NoteSpawner.cs
@@ -10,7 +10,9 @@ using Unity.VisualScripting;
 public class NoteSpawner : MonoBehaviour
 {
     public const string NOTESPAWNER_NAME = "Note Spawner";
-    public const string RESULT_SCENE_NAME = "lis result";
+    public const string RESULT_SCENE_NAME = "Lis Result";
+
+    [SerializeField] private bool isDebug = false;
 
     [Header("Note Reader")]
     [ReadOnly] public NoteReader reader;
@@ -32,6 +34,11 @@ public class NoteSpawner : MonoBehaviour
 
     private void Awake()
     {
+        if(isDebug)
+        {
+            if(Input.GetKeyUp(KeyCode.A)) { LisRead("Assets/Resources/Liss/PF.lis"); }
+        }
+
         GameObject @object = GameObject.Find(NOTESPAWNER_NAME);
         if (@object == null)
         {
@@ -39,6 +46,7 @@ public class NoteSpawner : MonoBehaviour
             if (GameObject.Find("System") != null) { @object.transform.parent = GameObject.Find("System").transform; }
 
             NoteSpawner spanwer = @object.AddComponent<NoteSpawner>();
+            spanwer.isDebug = isDebug;
 
             spanwer.reader = @object.GetComponent<NoteReader>();
             spanwer.timer = GameObject.Find(SoundTimer.SOUNDTIMER_NAME).GetComponent<SoundTimer>();
@@ -83,10 +91,7 @@ public class NoteSpawner : MonoBehaviour
         {
             endTime += Time.deltaTime;
             if(endTime > 3.0f) {
-                // Reset //
-                reader.isRead = false;
-                timer.Stop();
-                ///////////
+                SpawnerReset();
 
                 SceneManager.LoadScene(RESULT_SCENE_NAME);
             }
@@ -105,6 +110,13 @@ public class NoteSpawner : MonoBehaviour
     [ContextMenu("Note Queue Clear")]
     private void NoteClear()
     {
+        SpawnerReset();
         notes.Clear();
+    }
+
+    private void SpawnerReset()
+    {
+        reader.isRead = false;
+        timer.Stop();
     }
 }
